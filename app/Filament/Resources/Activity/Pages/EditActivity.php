b4ae83f0c8c96b6a91b6a39ec599d40c1339ae11
Jody <jody@example.com>
2026-06-15 19:49:26 +0700

    feat: add delete action to News and Activities tables and edit pages

--- app/Filament/Resources/Activity/Pages/EditActivity.php
+++ app/Filament/Resources/Activity/Pages/EditActivity.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Activity\Pages;
 
 use App\Filament\Resources\Activity\ActivityResource;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditActivity extends EditRecord
 {
     protected static string $resource = ActivityResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
 }
